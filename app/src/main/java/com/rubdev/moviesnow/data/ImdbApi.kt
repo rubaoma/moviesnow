17d91ab84172cd5a3d70957a462b89ae18c2d03d
Rubens <rubao.ma@gmail.com>
2021-03-11 22:40:05 -0300

    implementado o observer da api

--- app/src/main/java/com/rubdev/moviesnow/data/ImdbApi.kt
+++ app/src/main/java/com/rubdev/moviesnow/data/ImdbApi.kt
@@ -1,16 +1,18 @@
 package com.rubdev.moviesnow.data
 
 import com.rubdev.moviesnow.model.MovieResponse
+import io.reactivex.Flowable
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ImdbApi {
 
-    @GET("now_playing?api_key=&language=pt-BR&page=1")
+    @GET("now_playing")
+//    @GET("now_playing?api_key=&language=pt-BR&page=")
 
     fun getMoviesNowPlaying(
         @Query("api_key") apiKey: String,
         @Query("language") language: String,
         @Query("page") page: String
-    ): List<MovieResponse>
+    ): Flowable<MovieResponse>
 }
--- app/src/main/java/com/rubdev/moviesnow/repository/MoviesRepository.kt
+++ app/src/main/java/com/rubdev/moviesnow/repository/MoviesRepository.kt
@@ -8,7 +8,13 @@ import com.rubdev.moviesnow.data.ImdbApi
 import com.rubdev.moviesnow.di.DaggerAppComponent
 import com.rubdev.moviesnow.internal.KEY_IMDB
 import com.rubdev.moviesnow.model.MovieResponse
+import io.reactivex.Observable
+import io.reactivex.Scheduler
 import io.reactivex.disposables.Disposable
+import io.reactivex.disposables.Disposables
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.schedulers.Schedulers.io
+import io.reactivex.subscribers.DisposableSubscriber
 import javax.inject.Inject
 
 class MoviesRepository {
@@ -35,8 +41,27 @@ class MoviesRepository {
 
     private fun insertMovie(): Disposable {
         return iMDBApiService.getMoviesNowPlaying(KEY_IMDB, PT_BR, PAGE)
-            .sub
+            .subscribeOn(Schedulers.io())
+            .subscribeWith(subscribeToDatabase())
+    }
 
+    private fun subscribeToDatabase():DisposableSubscriber<MovieResponse>{
+        return object : DisposableSubscriber<MovieResponse>(){
+            override fun onNext(movieResponse: MovieResponse?) {
+                if (movieResponse != null){
+                    val entityList = movieResponse.results.toList()
+                }
+            }
+
+            override fun onError(t: Throwable?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onComplete() {
+                TODO("Not yet implemented")
+            }
+
+        }
     }
 
 }
