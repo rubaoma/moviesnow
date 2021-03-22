c0dad85604c47802518faf3f3d3eb4d8c73e66e9
Rubens <rubao.ma@gmail.com>
2021-03-22 00:08:04 -0300

    continuando a implementacao das funções de observacao

--- app/src/main/java/com/rubdev/moviesnow/view/ui/MainActivity.kt
+++ app/src/main/java/com/rubdev/moviesnow/view/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.rubdev.moviesnow.view.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.recyclerview.widget.DefaultItemAnimator
 import com.rubdev.moviesnow.R
+import com.rubdev.moviesnow.databinding.ActivityMainBinding
 import com.rubdev.moviesnow.di.DaggerAppComponent
 import com.rubdev.moviesnow.view.MovieAdapter
 import com.rubdev.moviesnow.viewmodel.MovieViewModel
@@ -11,27 +13,46 @@ import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     @Inject
     lateinit var movieAdapter: MovieAdapter
 
-     lateinit var biding:
+
     private val viewModel: MovieViewModel by viewModels()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         DaggerAppComponent.create().inject(this)
 
         setUpRecyclerView()
 
-//        observeLiveData()
+        observeLiveData()
     }
 
-    private fun setUpRecyclerView(){
-//        recycle_view.apply{
-
+    private fun setUpRecyclerView() {
+        binding.recycleView.apply {
+            setHasFixedSize(true)
+            itemAnimator = DefaultItemAnimator()
+            adapter = movieAdapter
         }
+    }
+
+    private fun observeLiveData() {
+        observeInProgress()
+        observeIsError()
+        observeMovieList()
+    }
+
+    private fun observeInProgress(){}
+
+    private fun observeIsError(){}
+
+    private fun observeMovieList(){}
 
 }
